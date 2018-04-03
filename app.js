6c7d0d4fd490f8894c3222b2d542655d9f142c75
Curtis Cawley <curtis.cawley@icloud.com>
2018-04-03 16:52:27 -0700

    fixed some var name errs and port is tested running

--- app.js
+++ app.js
@@ -25,6 +25,6 @@ app.use((req, res, next) => {
   res.status(404).json({ error: { message: 'Not found' } });
 });
 
-app.listen(port, () => console.log(`On port: ${port}`));
+app.listen(PORT, () => console.log(`Listening on port: ${PORT}`));
 
 module.exports = app;
