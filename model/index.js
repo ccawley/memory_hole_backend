af16f7fcaf15a9e8c09190126057d7b59caacf0e
Curtis Cawley <curtis.cawley@icloud.com>
2018-04-03 16:32:50 -0700

    renamed model folder to models

--- model/index.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  // Example from q3 below...
-
-  // User: require('./users.model'),
-  // Token: require('./token.model'),
-  // Commit: require('./commits.model'),
-  // Like: require('./likes.model')
-}
