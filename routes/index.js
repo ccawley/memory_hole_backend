2b1e1135ba902927b14fe0a1153405111bb6ff1b
Curtis Cawley <curtis.cawley@icloud.com>
2018-04-03 16:32:19 -0700

    added routes folder with skeleton for exporting eventual functions

---
+++ routes/index.js
@@ -0,0 +1,8 @@
+module.exports = {
+  // Example from q3 below...
+
+  // UsersRouter: require('./users.routes'),
+  // AuthRouter: require('./auth.routes'),
+  // CommitsRouter: require('./commits.routes'),
+  // LikesRouter: require('./likes.routes')
+}
