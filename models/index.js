6563f0ba853de52e45d06717bca2c20775984d30
Curtis Cawley <curtis.cawley@icloud.com>
2018-04-03 16:26:59 -0700

    added model folder with skeleton for exporting eventual functions

---
+++ models/index.js
@@ -0,0 +1,8 @@
+module.exports = {
+  // Example from q3 below...
+
+  // User: require('./users.model'),
+  // Token: require('./token.model'),
+  // Commit: require('./commits.model'),
+  // Like: require('./likes.model')
+}
