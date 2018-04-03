df0ac80d9c23032a2694ada0fd4dedef0947146e
Curtis Cawley <curtis.cawley@icloud.com>
2018-04-03 16:31:51 -0700

    added controller folder with skeleton for exporting eventual functions

---
+++ controllers/index.js
@@ -0,0 +1,8 @@
+module.exports = {
+  // Example from q3 below...
+
+  // UsersController: require('./users.controller'),
+  // AuthController: require('./auth.controller'),
+  // CommitsController: require('./commits.controller'),
+  // LikesController: require('./likes.controller')
+}
