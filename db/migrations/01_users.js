0a9829ae7df35d7842229d962802206d8f97c18b
Curtis Cawley <curtis.cawley@icloud.com>
2018-04-04 13:07:51 -0700

    created a users table and migrations

---
+++ db/migrations/01_users.js
@@ -0,0 +1,15 @@
+exports.up = function(knex, Promise) {
+  return knex.schema.createTable('users', (table) => {
+    table.increments('id')
+    table.string('user_name').notNullable().defaultTo('')
+    table.string('first_name').notNullable().defaultTo('')
+    table.string('password').notNullable().defaultTo('')
+    table.string('email').notNullable().defaultTo('')
+    table.image('avatar_image')
+    table.timestamps(true, true)
+  })
+};
+
+exports.down = function(knex, Promise) {
+  return knex.schema.dropTable('users')
+};
