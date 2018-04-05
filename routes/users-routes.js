09823a6889543dad011b577b08413554344198f1
Curtis Cawley <curtis.cawley@icloud.com>
2018-04-04 20:24:33 -0700

    changed name of route of the get user method

--- routes/users-routes.js
+++ routes/users-routes.js
@@ -2,6 +2,6 @@ const express = require('express')
 const router = express.Router()
 const { UsersController } = require('../controllers')
 
-router.get('/all', UsersController.message)
+router.get('/userName', UsersController.message)
 
 module.exports = router
