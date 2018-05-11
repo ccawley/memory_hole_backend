649e6d38ef4105b85f4d4168e7f50f380d870f39
Curtis Cawley <curtis.cawley@icloud.com>
2018-05-11 15:01:32 -0700

    cleaned up and bolstered client responses for the login process

--- controllers/users-controller.js
+++ controllers/users-controller.js
@@ -23,18 +23,24 @@ class UsersController {
       .then(newUser => {
         return res.status(201).json({ message: "New user succesfully created."})
       })
-      .catch(console.error)
+      .catch(error => {
+        return next(error)
+      })
   }
 
   static login (req, res, next) {
     let { user_name, password } = req.body
 
     if (!user_name || !password) {
-      return res.status(401).json({ message: "Username and password required." })
+      return next ({ status: 400, message: "Username and password required." })
     } else {
       User.tryLoginUser(user_name, password)
       .then(user => {
-        if (user === false) return next({ status: 400, message: "Incorrect password." })
+        if (user === false) {
+          return next({ status: 401, message: "Incorrect password." })
+        } else if (user === 'User does not exist.') {
+          return next({ status: 401, message: "User does not exist."})
+        }
         return res.status(200).json(user)
       })
       .catch(error => {
